0aafb1a39683f0ee018bb19262f17868f2442479
Yacine Mahdid <yacine.mahdid@mail.mcgill.ca>
2023-05-19 22:33:58 -0400

    Fix the creation of the dictionary tree

--- src/create_dict_tree.py
+++ src/create_dict_tree.py
@@ -1,9 +1,9 @@
 # script to create a tree structure of the dictionary
 
-# TODO: Add is_word to Node class
-# TODO: Add sequence to Node class to store words (might not be 100% required but useful for debugging)
-# TODO: Absorb the first layer of iteration into the recursive function, doesn't make sense to have duplicated code in the main
+# TODO: Save the tree in the repository so that I can load it in the main function
 # TODO: [Optional] Fix up maybe how we represent letters, it's a bit all over the place currently and hard-coded for English
+
+# BUG: Depth variable seems to be absolutely useless
 
 import json
 
@@ -12,24 +12,23 @@ DICTIONARY_NAME = 'src/en_dict.txt'
 # Letters that are supported 
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+global WORD_COUNT
+WORD_COUNT = 0
+
 # Node that composed the tree structure that will be traverse
 class Node:
     letter = ''
     edges = [] #node can have up to 26 edges with current english alphabet
     depth = 0
+    is_word = False
+    sequence = ""
 
-    def __init__(self, letter, depth):
+    def __init__(self, letter, depth, sequence, is_word):
         self.letter = letter
         self.edges = []
         self.depth = depth
-
-    # Print fucntion that traverse the whole tree
-    def traverse_tree(self):
-        spaces = ' '*self.depth
-        print(spaces + self.letter)
-
-        for node in self.edges:
-            node.traverse_tree()
+        self.sequence = sequence
+        self.is_word = is_word
 
 
 def get_sub_dict(dict, sequence):
@@ -48,6 +47,13 @@ def get_sub_dict(dict, sequence):
 
     return sub_dict    
 
+def is_sequence_a_word(dict, sequence):
+
+    for word in dict:
+        if word == sequence:
+            return True
+    return False
+
 def get_longest_word(dict):
     """ return the longest word out of the dictionary list O(n)"""
 
@@ -60,12 +66,11 @@ def get_longest_word(dict):
     return longest_word_len
 
 
-def recurse_letters_all(letter_i, sequence, dict, sub_tree, cur_depth, max_depth):
+def recurse_letters_all(sequence, dict, sub_tree, cur_depth, max_depth):
     """
         Recursively create and add nodes in the alphabetical decision tree.
         
-        letter_i: index of the letter currently being used
-        sequence: the sequence of characters that make up a potential word
+        sequence: current sequence of character up to this point
         dict: the current sub dictionary that could match the sequence
         sub_tree: the tree onto which this letter originated and upon which nodes will be appended
         cur_depth: the current depth of the letter in the full tree.
@@ -74,28 +79,39 @@ def recurse_letters_all(letter_i, sequence, dict, sub_tree, cur_depth, max_depth
         return nothing as it only populate the nodes in the tree
     """
 
+    #print(f"{cur_depth}")
+
     # recursion stop when we hit the max depth allowed
     if cur_depth == max_depth:
         return
 
-    # creation of the new sequence of letters and filtering of the dictionary to match
-    new_sequence = sequence + LETTERS[letter_i]
     new_depth = cur_depth + 1
-    new_sub_dict = get_sub_dict(dict, new_sequence)
 
-    # if there is something matching this sequence we continue recursing
-    # otherwise we stop as this sequence is depleted from available characters
-    if new_sub_dict:
-                
-        # Iterate on each letters of the alphabet
-        for letter_i in range(26):
+    # Iterate on each letters of the alphabet
+    for letter_i in range(26):
+
+        # creation of the new sequence of letters and filtering of the dictionary to match
+        new_sequence = sequence + LETTERS[letter_i]
+        new_sub_dict = get_sub_dict(dict, new_sequence)
+
+        # if there is something matching this sequence we continue recursing
+        # otherwise we stop as this sequence is depleted from available characters
+        if new_sub_dict:
             
+            # check if this sequence is a word
+            is_word = is_sequence_a_word(new_sub_dict, new_sequence)
+
+            if is_word:
+                global WORD_COUNT
+                WORD_COUNT = WORD_COUNT + 1
+                print(f"Total number of word = {WORD_COUNT} and word is {new_sequence}")
+
             # Create the node and append it to the tree
-            node = Node(LETTERS[letter_i], new_depth)
+            node = Node(LETTERS[letter_i], new_depth, new_sequence, is_word)
             sub_tree.edges.append(node)
             
             # Recurse with the new parameters
-            recurse_letters_all(letter_i, new_sequence, new_sub_dict, node, new_depth, max_depth)
+            recurse_letters_all(new_sequence, new_sub_dict, node, new_depth, max_depth)
 
 if __name__ == '__main__':
     # Load the dictionary so that we can manipulate it
@@ -108,16 +124,7 @@ if __name__ == '__main__':
     max_depth = get_longest_word(file_data)
 
     # The tree that will contains all
-    tree = Node('', 0)
+    tree = Node('', 0, '', False)
 
-    # Iterate on the first layer of the tree
-    for letter_i in range(26):
-        
-        print(f"{letter_i}")
-
-        node = Node(LETTERS[letter_i], 1)
-        tree.edges.append(node)
-
-        recurse_letters_all(letter_i, '', file_data, node, 0, max_depth)
-
-    tree.traverse_tree()
+    # Populate the tree
+    recurse_letters_all('', file_data, tree, 0, max_depth)
